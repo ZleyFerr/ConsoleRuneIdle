078f9b09de237a922bcccdbfe72fac94270df715
ZleyFerr <78102828+ZleyFerr@users.noreply.github.com>
2026-07-03 01:56:33 +0300

    Add files via upload

---
+++ source/Menu/menu_fill.cpp
@@ -0,0 +1,8 @@
+#include "menu_fill.h"
+
+void fillMenu(gameMenu& menu)
+{
+	menu.addLine("Exit game");
+	menu.addLine("Farm runeCurrency");
+	menu.addLine("Roll basic rune and basic rune stats");
+}
